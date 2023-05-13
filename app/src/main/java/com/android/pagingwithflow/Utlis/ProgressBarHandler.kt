2d6118d1bb24de9c32b782fc70af044709fc3354
Dinesh2510 <dineshchavan7708@outlook.com>
2023-05-14 01:31:57 +0530

    class into kotlin

--- app/src/main/java/com/android/pagingwithflow/Utlis/ProgressBarHandler.kt
+++ app/src/main/java/com/android/pagingwithflow/Utlis/ProgressBarHandler.kt
@@ -1,43 +1,37 @@
-package com.android.pagingwithflow.Utlis;
+package com.android.pagingwithflow.Utlis
 
-import android.app.Activity;
-import android.content.Context;
-import android.view.Gravity;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ProgressBar;
-import android.widget.RelativeLayout;
+import android.R
+import android.app.Activity
+import android.content.Context
+import android.view.Gravity
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.RelativeLayout
 
-public class ProgressBarHandler {
-    private ProgressBar mProgressBar;
-    private Context mContext;
+class ProgressBarHandler(private val mContext: Context) {
+    private val mProgressBar: ProgressBar
 
-    public ProgressBarHandler(Context context) {
-        mContext = context;
-
-        ViewGroup layout = (ViewGroup) ((Activity) context).findViewById(android.R.id.content).getRootView();
-
-        mProgressBar = new ProgressBar(context, null, android.R.attr.progressBarStyleLarge);
-        mProgressBar.setIndeterminate(true);
-
-        RelativeLayout.LayoutParams params = new
-                RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
-
-        RelativeLayout rl = new RelativeLayout(context);
-
-        rl.setGravity(Gravity.CENTER);
-        rl.addView(mProgressBar);
-
-        layout.addView(rl, params);
-
-        hide();
+    init {
+        val layout = (mContext as Activity).findViewById<View>(R.id.content).rootView as ViewGroup
+        mProgressBar = ProgressBar(mContext, null, R.attr.progressBarStyleLarge)
+        mProgressBar.isIndeterminate = true
+        val params = RelativeLayout.LayoutParams(
+            RelativeLayout.LayoutParams.MATCH_PARENT,
+            RelativeLayout.LayoutParams.MATCH_PARENT
+        )
+        val rl = RelativeLayout(mContext)
+        rl.gravity = Gravity.CENTER
+        rl.addView(mProgressBar)
+        layout.addView(rl, params)
+        hide()
     }
 
-    public void show() {
-        mProgressBar.setVisibility(View.VISIBLE);
+    fun show() {
+        mProgressBar.visibility = View.VISIBLE
     }
 
-    public void hide() {
-        mProgressBar.setVisibility(View.INVISIBLE);
+    fun hide() {
+        mProgressBar.visibility = View.INVISIBLE
     }
 }
